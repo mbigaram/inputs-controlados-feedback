8b2f7d5e7b1fff53ca07f3193db1d44ec1f15aa0
mbiga <mbigaram.mb@gmail.com>
2022-10-18 18:19:43 -0300

    Exercicios

--- src/Components/ConfirmationForm/ConfirmationForm.js
+++ src/Components/ConfirmationForm/ConfirmationForm.js
@@ -1,12 +1,67 @@
 import React from 'react'
-import { Form} from '../MainPage/styles'
+import { Form } from '../MainPage/styles'
 
-const ConfirmationForm = () => {
+
+// const ConfirmationForm = () => {
+//   const [data, setData] = useState("");
+//   const onChangeData = (event) => {
+//   console.log(event.target.value)
+//   }
+
+// }
+
+const ConfirmationForm = (props) => {
+
+  const enviarDados=()=>{
+    alert("Dados Enviados")
+  }
+
   return (
     <Form>
-        
-      <button>Enviar dados</button>
-      </Form>
+      <h2>exercicio 3</h2>
+
+      <label>
+        Front-End:
+        <input type="radio" />
+      </label>
+      <label>
+        Back-End:
+        <input type="radio" />   
+      </label>
+
+      <label>
+      Data de nascimento:
+        <input  type="date"/>
+      </label>
+
+      <label>
+     
+        <select>
+        <option selected value="Genero">Genero</option>
+          <option>Homem</option>
+          <option>Mulher</option>
+        </select>
+      </label>
+      <label>
+      <select>
+      <option selected value="Raça">Raça</option>
+        <option value="Raça Negra">Negro</option>
+        <option value="Raça Amarela">Amarelão</option>
+        <option value="Raça Branca">Branco</option>
+        <option value="Raça Vermelha">Vermelho</option>
+      </select>
+      </label>
+
+      <label for="vehicle1">Tem ensino médio completo
+      <input type="checkbox"/><br></br>
+      </label>
+
+      <label for="vehicle1">Tem disponibilidade para participar das atividades
+      <input type="checkbox"/><br></br>
+      </label>
+      <button onClick={enviarDados}>Enviar dados</button>
+
+    </Form>
   )
 }
 
--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,35 +1,52 @@
-import React, {useState} from 'react'
-import {MainContainer} from './styles'
+import React, { useState } from 'react'
+import { MainContainer } from './styles'
 import NameForm from '../NameForm/NameForm'
 import ConfirmationForm from '../ConfirmationForm/ConfirmationForm'
 
 const MainPage = () => {
-const [name, setName] = useState("")
-const [age, setAge] = useState("")
-const [email, setEmail] = useState("")
-const [formFlow, setFormFlow] = useState(1) //você não precisa mexer neste estado, ele faz parte da lógica da linha 30 do JSX
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [email, setEmail] = useState("")
+  const [formFlow, setFormFlow] = useState(1)
+  const [confirmaEmail, setConfirmaEmail]= useState("") //você não precisa mexer neste estado, ele faz parte da lógica da linha 30 do JSX
 
-const onChangeName = (event) => {
-  setName(event.target.value)
-}
+  const onChangeName = (event) => {
+    setName(event.target.value)
+  }
 
-const onChangeAge = (event) => {
-  setAge(event.target.value)
-}
+  const onChangeAge = (event) => {
+    setAge(event.target.value)
+  }
 
-const onChangeEmail = (event) => {
-  setEmail(event.target.value)
-}
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value)
+  }
 
-const sendData = () => {
-  //aqui deve vir uma verificação para mudar de formulario apenas se todos os requisitos tiverem sido cumpridos
-  setFormFlow(2)
-}
+  const onChangeConfirmaEmail = (event) => {
+    setConfirmaEmail(event.target.value)
+  }
+
+  const sendData = () => {
+   if(age>=18 && name.length >=10 && name.length<=30 && email.includes("@") && email===confirmaEmail){
+
+    setFormFlow(2)
+   }else{
+    alert("Não respondeu o formulario")
+   }
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
-      {formFlow === 1 ? <NameForm
-      // insira aqui suas props
+      {formFlow === 1 ? <NameForm 
+      name={name}
+      age={age}
+      email={email}
+      onChangeName={onChangeName}
+      onChangeAge={onChangeAge}
+      onChangeEmail={onChangeEmail}
+      onChangeConfirmaEmail={onChangeConfirmaEmail}
+      sendData={sendData}  
       /> : <ConfirmationForm />}
     </MainContainer>
   )
